745cd8fa323281a9029709a4c822e391ac740f73
Shlokabesec <honeyshlok@gmail.com>
2026-06-18 17:06:27 +0530

    .

---
+++ condition.js
@@ -0,0 +1,77 @@
+// let age = 3;
+// if (age < 18) {
+//     console.log("You are a minor.");
+// }else if (age >= 18 && age < 65) {
+//     console.log("You are an adult.");
+// }else {
+//     console.log("You are a senior.");
+// }
+// switch (age) {
+//     case 0:
+//         console.log("You are a newborn.");
+//         break;
+//     case 1:
+//         console.log("You are an infant.");
+//         break;
+//     case 2:
+//         console.log("You are a toddler.");
+//         break;
+//     case 3:
+//         console.log("You are a preschooler.");
+//         break;
+//     case 4:
+//         console.log("You are a kindergartener.");
+//         break;
+//     case 5:
+//         console.log("You are a grade schooler.");
+//         break;
+//     case 6:
+//         console.log("You are a middle schooler.");
+//         break;
+//     case 7:
+//         console.log("You are a high schooler.");
+//         break;
+//     case 8:
+//         console.log("You are a college student.");
+//         break;
+//     case 9:
+//         console.log("You are a working professional.");
+//         break;
+//     default:
+//         console.log("You are an adult.");
+//         break;
+// }
+// let day = "Monday";
+// if (day === "Monday") {
+//     console.log("Today is Monday.");
+// } else if (day === "Tuesday") {
+//     console.log("Today is Tuesday.");
+// }else {
+//     console.log("Today is not Monday or Tuesday.");
+// }
+// let day = 3;
+// switch (day) {
+//     case 1:
+//         console.log("Today is Monday.");
+//         break;
+//     case 2:
+//         console.log("Today is Tuesday.");
+//         break;
+//     case 3:
+//         console.log("Today is Wednesday.");
+//         break;
+//     case 4:
+//         console.log("Today is Thursday.");
+//         break;
+//     case 5:
+//         console.log("Today is Friday.");
+//         break;
+//     case 6:
+//         console.log("Today is Saturday.");
+//         break;
+//     case 7:
+//         console.log("Today is Sunday.");
+//         break;
+//     default:
+//         console.log("Invalid day number.");
+// }
